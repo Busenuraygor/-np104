4c3fca1a0caa0b26e2507672d2f28012bd26e056
Busenuraygor <busenur.aygor@std.yeditepe.edu.tr>
2025-04-28 17:36:33 +0300

    Add files via upload
    28/4/25

--- soru2.py
+++ soru2.py
@@ -1,3 +1,7 @@
-ad = input("Adınızı girin:")
-soyad= input("Soyadınızı giriniz")
-print(ad[0].upper() + "." + soyad[0].upper())
+p1=(0,0)
+p2=(3,4)
+
+dx=p2[0] - p1[0]
+dy= p2[1] - p1[1]
+mesafe= (dx**2 + dy**2)**0.5
+print(mesafe)
